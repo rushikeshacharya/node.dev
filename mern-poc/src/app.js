b7bc2af505650aaf8510285e6c28c59db5725326
Rushikesh Acharya <rushikeshacharya09@gmail.com>
2025-02-26 11:38:36 +0530

    feat: basic CRUD API's

--- mern-poc/src/app.js
+++ mern-poc/src/app.js
@@ -28,22 +28,64 @@ const User = require("./models/user");
 // app.use("/", (req, res) => {
 //   return res.send("Hello ....");
 // });
-
+app.use(express.json());
 app.post("/signup", async (req, res) => {
   const user = new User({
-    firstName: "Flash",
-    lastName: "India",
-    emailId: "flash@india.in",
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    emailId: req.body.emailId,
+    password: req.body.password,
   });
 
   try {
     await user.save();
     res.send("User Saved Successfully");
   } catch (error) {
-    res.status(400).send("Errior saving user");
+    res.status(400).send(`Errior saving user: ${error}`);
   }
 });
 
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).send(users);
+  } catch (error) {}
+});
+
+app.get("/user", async (req, res) => {
+  try {
+    const user = await User.find({ emailId: req.body.emailId });
+    // const user = await User.findOne({ emailId: req.body.emailId });
+
+    if (user.length === 0) {
+      res.status(404).send("User not found");
+    }
+    res.send(user);
+  } catch (error) {}
+});
+
+app.patch('/user', async (req, res) => {
+    const userDetails = req.body;
+    try {
+      console.log('', userDetails);
+      const user = await User.findByIdAndUpdate({ _id: userDetails.userId}, userDetails);
+      console.log("User----->", user);
+      res.send(user);
+      
+    } catch (error) {
+        console.log("Error ", error);
+        
+    }
+})
+
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    await User.findByIdAndDelete(userId);
+    // const user = await User.findByIdAndDelete({_id: userId});
+    res.status(200).send("User Deleted Successfully");
+  } catch (error) {}
+});
 connectDB()
   .then(() => {
     console.log("Connected.....");
--- mern-poc/src/models/user.js
+++ mern-poc/src/models/user.js
@@ -1,25 +1,77 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 4,
+      maxLength: 50,
+    },
+    lastName: {
+      type: String,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      lowercase: true,
+      unique: true,
+      trim: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid email address: " + value);
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default: "https://geographyandyou.com/images/user-profile.png",
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error("Invalid Photo URL: " + value);
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error("Enter a strong password: " + value);
+        }
+      },
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      enum: {
+        values: ["male", "female", "other"],
+        message: `{VALUE} is not a valid gender type`,
+      },
+    },
+    isPremium: {
+      type: Boolean,
+      default: false,
+    },
+    membershipType: {
+      type: String,
+    },
+    about: {
+      type: String,
+      default: "This is a default about the user!",
+    },
+    skills: {
+      type: [String],
+    },
   },
-  lastName: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: String,
-  },
-  gender: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const User = mongoose.model("User", userSchema);
 
