26a193f004d7c4262e035cafbc31c3468f1e459e
Rushikesh Acharya <rushikeshacharya09@gmail.com>
2025-05-18 01:02:41 +0530

    feat: accept / reject profiles

--- mern-poc/web/src/components/Feed.jsx
+++ mern-poc/web/src/components/Feed.jsx
@@ -24,11 +24,15 @@ const Feed = () => {
     getFeed();
     return () => {};
   }, []);
+  if (!feed) return;
+
+  if (feed.length <= 0)
+    return <h1 className="flex justify-center my-10">No new users founds!</h1>;
 
   return (
     feed && (
       <div className="flex justify-center my-10">
-        <UserCard user={feed[1]} />
+        <UserCard user={feed[0]} />
       </div>
     )
   );
--- mern-poc/web/src/components/UserCard.jsx
+++ mern-poc/web/src/components/UserCard.jsx
@@ -1,14 +1,26 @@
-import React from "react";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { removeUserFromFeed } from "../utils/feedSlice";
 
-const UserCard = ({user}) => {
-    const {photoUrl, about, firstName, lastName, age, gender, skills} = user;
-    
+const UserCard = ({ user }) => {
+  const { _id, photoUrl, about, firstName, lastName, age, gender, skills } =
+    user;
+  const dispatch = useDispatch();
+  const handleSendRequest = async (status, userId) => {
+    try {
+      const res = await axios.post(
+        `${BASE_URL}/request/send/${status}/${userId}`,
+        {},
+        { withCredentials: true }
+      );
+      dispatch(removeUserFromFeed(userId));
+    } catch (error) {}
+  };
   return (
     <div className="card bg-base-300 w-96 shadow-sm">
       <figure>
-        <img
-          src= {photoUrl}
-        />
+        <img src={photoUrl} />
       </figure>
       <div className="card-body">
         <h2 className="card-title">{`${firstName}, ${lastName}`}</h2>
@@ -16,9 +28,18 @@ const UserCard = ({user}) => {
         <h4 className="card-title text-sm">{`Skills: ${skills}`}</h4>
         <p>{about}</p>
         <div className="card-actions justify-center my-4">
-          <button className="btn btn-primary">👎</button>
-          <button className="btn btn-secondary">👍</button>
-
+          <button
+            className="btn btn-primary"
+            onClick={() => handleSendRequest("ignored", _id)}
+          >
+            👎
+          </button>
+          <button
+            className="btn btn-secondary"
+            onClick={() => handleSendRequest("interested", _id)}
+          >
+            👍
+          </button>
         </div>
       </div>
     </div>
